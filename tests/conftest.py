800331ef2994de392154ade53f35600d01ec81cd
olenkasalo <olena_salo@epam.com>
2023-07-13 06:55:47 -0700

    browser_config: add browser and page fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import pytest
+from playwright.sync_api import sync_playwright
 
 from src.app import Application
 
@@ -25,6 +26,21 @@ def get_config(request):
         return read_ini()[env_name]
     except KeyError:
         raise Exception(f"Wrong configuration for env name [{env_name}] NOT present")
+
+@pytest.fixture(scope="session")
+def browser(request):
+    with sync_playwright() as playwright:
+        browser = playwright[get_config(request)['browser_name']].launch(headless=False)
+        yield browser
+        browser.close()
+
+@pytest.fixture(scope="session")
+def page(request, browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(get_config(request)['base_url'])
+    yield page
+    context.close()
 
 @pytest.fixture
 def app(page, request):
